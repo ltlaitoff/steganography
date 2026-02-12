c9cfe9c8f61762ac6795c6928e279aa9b4dc00ad
Shchedrovskyi Ivan <ltlaitoff@gmail.com>
2026-02-12 09:26:36 +0200

    Format all files with prettier

--- website/wasm_exec.d.ts
+++ website/wasm_exec.d.ts
@@ -3,11 +3,11 @@
  * Golang WebAssembly wiki: https://github.com/golang/go/wiki/WebAssembly
  */
 declare class Go {
-    argv: string[];
-    env: { [envKey: string]: string };
-    exit: (code: number) => void;
-    importObject: WebAssembly.Imports;
-    exited: boolean;
-    mem: DataView;
-    run(instance: WebAssembly.Instance): Promise<void>;
+	argv: string[]
+	env: { [envKey: string]: string }
+	exit: (code: number) => void
+	importObject: WebAssembly.Imports
+	exited: boolean
+	mem: DataView
+	run(instance: WebAssembly.Instance): Promise<void>
 }
